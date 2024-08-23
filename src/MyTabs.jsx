05d564a7207387343d71425f298b64dc34c8ac65
mohmdrezaei <mhwmdrz9@gmail.com>
2024-08-24 01:23:33 +0330

    Fix some bugs

--- src/MyTabs.jsx
+++ src/MyTabs.jsx
@@ -3,40 +3,37 @@ import "./MyTabs.css";
 
 function MyTabs() {
   const [activeTab, setActiveTab] = useState(
-    () => localStorage.getItem("activeTab") || "tab1"
+    () => localStorage.getItem("activeTab") || "Tab1"
   );
   const [tabs, setTabs] = useState(
-    JSON.parse(localStorage.getItem("tabs")) || ["tab1", "tab2", "tab3", "tab4"]
+    JSON.parse(localStorage.getItem("tabs")) || ["Tab1", "Tab2", "Tab3", "Tab4"]
   );
 
   const handleTabClick = (tabId) => {
-    
     setActiveTab(tabId);
     localStorage.setItem("activeTab", tabId);
   };
 
   const handleAddTab = () => {
-    const newTabId = `tab${tabs.length + 1}`;
-    if (tabs.length === 8) {
-      return alert("نمی توانید بیشتر از 8 برگه اینجاد کنید");
-    }
-    setTabs([...tabs, newTabId]);
+    const maxId = tabs.reduce((max, tab) => Math.max(max, JSON.parse(tab.replace("Tab", ""), 10)),0);
+    const newTabId = `Tab${maxId + 1}`;
+    const updatedTabs = [...tabs, newTabId];
+    setTabs(updatedTabs);
     setActiveTab(newTabId);
+    localStorage.setItem("tabs", JSON.stringify(updatedTabs));
     localStorage.setItem("activeTab", newTabId);
-    localStorage.setItem("tabs", JSON.stringify([...tabs, newTabId]));
   };
-  const handleDeleteTab = (e ,tabId) => {
+  const handleDeleteTab = (e, tabId) => {
     e.stopPropagation();
-    if(tabs.length === 1){
+    if (tabs.length === 1) {
       return alert("حداقل یک برگه باید باز باشد");
     }
     const updatedTabs = tabs.filter((tab) => tab !== tabId);
-    console.log(updatedTabs)
-    setTabs(updatedTabs );
-    setActiveTab(updatedTabs[updatedTabs.length - 1])
-    localStorage.setItem('tabs', JSON.stringify(updatedTabs));
-    localStorage.setItem('activeTab',updatedTabs[updatedTabs.length - 1]);
-  
+    console.log(updatedTabs);
+    setTabs(updatedTabs);
+    setActiveTab(updatedTabs[updatedTabs.length - 1]);
+    localStorage.setItem("tabs", JSON.stringify(updatedTabs));
+    localStorage.setItem("activeTab", updatedTabs[updatedTabs.length - 1]);
   };
 
   return (
@@ -50,7 +47,12 @@ function MyTabs() {
             onClick={() => handleTabClick(tabId)}
           >
             {tabId}
-             <button className="delete-button" onClick={(e) => handleDeleteTab(e ,tabId)}>&#10006;</button>
+            <button
+              className="delete-button"
+              onClick={(e) => handleDeleteTab(e, tabId)}
+            >
+              &#10006;
+            </button>
           </button>
         ))}
         <button className="add-button" onClick={handleAddTab}>
