ab548dc204f3c247103edae92158bb41316f83e5
mohmdrezaei <mhwmdrz9@gmail.com>
2024-08-23 17:24:27 +0330

    Ability to add a new tab by the user

--- src/MyTabs.jsx
+++ src/MyTabs.jsx
@@ -2,32 +2,47 @@ import { useState } from "react";
 import './MyTabs.css';
 
 function MyTabs() {
-  const [activeTab, setActiveTab] = useState(()=>localStorage.getItem('activeTab') || "tab1");
+  const [activeTab, setActiveTab] = useState(() => localStorage.getItem('activeTab') || "tab1");
+  const [tabs, setTabs] = useState( JSON.parse(localStorage.getItem('tabs')) ||["tab1", "tab2", "tab3","tab4"]
+  );
 
   const handleTabClick = (tabId) => {
     setActiveTab(tabId);
     localStorage.setItem('activeTab', tabId);
   };
 
+  const handleAddTab = () => {
+    const newTabId = `tab${tabs.length + 1}`;
+    if(tabs.length === 8) {
+      return alert("نمی توانید بیشتر از 8 برگه اینجاد کنید")
+    }
+    setTabs([...tabs, newTabId]);
+    setActiveTab(newTabId);
+    localStorage.setItem('activeTab', newTabId);
+    localStorage.setItem('tabs', JSON.stringify([...tabs, newTabId]));
+  };
+
   return (
     <div className="tab-container">
-      <h1>tabs Component with react</h1>
+      <h1>Tabs Component with react</h1>
       <div className="tab-list">
-        <button className={`tab-link ${activeTab === 'tab1' ? 'active' : ''}`} onClick={() => handleTabClick('tab1')}>Tab 1</button>
-        <button className={`tab-link ${activeTab === 'tab2' ? 'active' : ''}`} onClick={() => handleTabClick('tab2')}>Tab 2</button>
-        <button className={`tab-link ${activeTab === 'tab3' ? 'active' : ''}`} onClick={() => handleTabClick('tab3')}>Tab 3</button>
-        <button className={`tab-link ${activeTab === 'tab4' ? 'active' : ''}`} onClick={() => handleTabClick('tab4')}>Tab 4</button>
+        {tabs.map((tabId) => (
+          <button
+            key={tabId}
+            className={`tab-link ${activeTab === tabId ? 'active' : ''}`}
+            onClick={() => handleTabClick(tabId)}>
+             {tabId}
+          </button>
+        ))}
+        <button className="add-button" onClick={handleAddTab}>+</button>
       </div>
       <div className="tab-content">
-        {activeTab === 'tab1' && <h2>Content 1</h2>}
-        {activeTab === 'tab2' && <h2>Content 2</h2>}
-        {activeTab === 'tab3' && <h2>Content 3</h2>}
-        {activeTab === 'tab4' && <h2>Content 4</h2>}
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Deleniti similique natus sed blanditiis. Nam quibusdam soluta, 
-            fugiat a reiciendis molestiae inventore. Doloremque totam
-             praesentium sequi nam veniam aut provident atque.
-             </p>
+        {activeTab && (
+          <div>
+            <h2>Content {activeTab}</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti similique natus sed blanditiis. Nam quibusdam soluta, fugiat a reiciendis molestiae inventore. Doloremque totam praesentium sequi nam veniam aut provident atque.</p>
+          </div>
+        )}
       </div>
     </div>
   );
